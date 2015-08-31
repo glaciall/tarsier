3807cff821cf522e5aa8a580158caa4a3aff6c12
houcheng <houcheng@ui.boyuan.com>
2015-08-31 09:26:58 +0800

    给strike添加中划线效果

--- Tarsier/src/cn/org/hentai/tarsier/view/text/Strike.java
+++ Tarsier/src/cn/org/hentai/tarsier/view/text/Strike.java
@@ -1,7 +1,20 @@
 package cn.org.hentai.tarsier.view.text;
 
+import cn.org.hentai.tarsier.graphic.Brush;
+import cn.org.hentai.tarsier.graphic.Image;
+import cn.org.hentai.tarsier.graphic.Painter;
+
 // tag <strike>
 public class Strike extends Text
 {
-
+	@Override
+	protected void onDraw(Image image)
+	{
+		super.onDraw(image);
+		Painter painter = image.getPainter();
+		Brush brush = new Brush();
+		brush.setColor(0xff000000);
+		painter.setBrush(brush);
+		painter.drawLine(0, this.layout.height / 2, this.layout.width, this.layout.height / 2);
+	}
 }
